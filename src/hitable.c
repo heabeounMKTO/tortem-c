5d54833dd69fa221776767251da65b8d08433575
heabeounMKTO <heabeoun69zin@gmail.com>
2024-08-27 16:09:28 +0700

    hitable list

--- src/hitable.c
+++ src/hitable.c
@@ -24,3 +24,46 @@ void free_hit_record(HitRecord* record) {
     free(record);
   }
 }
+
+HitableList* init_hit_record_list(size_t initial_capacity) {
+  HitableList* list = (HitableList*)malloc(sizeof(HitableList));
+  if (list == NULL) {
+    return NULL;
+  }
+  list->records = (HitRecord**)malloc(initial_capacity * sizeof(HitRecord*));
+  if (list->records==NULL) {
+    free(list);
+    return NULL;
+  }
+  list->size = 0;
+  list->capacity = initial_capacity;
+  return list;
+}
+
+
+bool add_record_to_hitablelist(HitableList *list, HitRecord *record) {
+  if (list->size == list->capacity) {
+    size_t new_capacity = list->capacity * 2;
+
+    // resize that bih
+    HitRecord** new_records = (HitRecord**)realloc(list->records, new_capacity * sizeof(HitRecord*));
+    if(new_records==NULL) {
+      return false;
+    }
+    list->records = new_records;
+    list->capacity=new_capacity;
+  }
+  list->records[list->size++] = record;
+  return true;
+}
+
+void free_hit_record_list(HitableList* list) {
+    if (list == NULL) return;
+
+    for (size_t i = 0; i < list->size; i++) {
+        free_hit_record(list->records[i]);      }
+
+    free(list->records);  
+    free(list);           
+}
+
--- src/hitable.h
+++ src/hitable.h
@@ -16,12 +16,17 @@ HitRecord* new_hit_record(Vec3 point, Vec3 normal, double t,bool front_face, boo
 HitRecord* no_hits();
 void free_hit_record(HitRecord* record);
 
+// TODO: calcuate length of hit record list
+// on start instead ion like this 
+// 𝓭𝔂𝓷𝓪𝓶𝓲𝓬 𝓪𝓻𝓻𝓪𝔂 nonsense
 typedef struct {
   HitRecord** records;
   size_t size;
   size_t capacity;
 } HitableList;
 
-
+HitableList* init_hit_record_list(size_t initial_capacity);
+bool add_record_to_hitablelist(HitableList* list, HitRecord* record);
+void free_hit_record_list(HitableList* list); 
 
 #endif
