9789972c20194a011e751eac61abb5bbfee3cb45
heabeounMKTO <heabeoun69zin@gmail.com>
2024-08-27 17:35:12 +0700

    inti cam

--- src/camera.c
+++ src/camera.c
@@ -9,6 +9,34 @@ Vec3 sample_sq() {
   return random_vec;
 }
 
+Camera* new_cam(int image_width, int image_height) {
+  double aspect_ratio = (double) image_width / (double) image_height;
+  double focal_length = 1.0;
+  double viewport_height = 2.0;
+  double viewport_width = viewport_height * ((double)image_width / image_height);
+  Vec3 camera_center = {0, 0, 0};
+
+  // Calculate the vectors across the horizontal and down the vertical viewport edges.
+  Vec3 viewport_u = {viewport_width, 0, 0};
+  Vec3 viewport_v = {0, -viewport_height, 0};
+
+  // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+  Vec3 pixel_delta_u = div_vec3(viewport_u, int2vec(image_width));
+  Vec3 pixel_delta_v = div_vec3(viewport_v, int2vec(image_height));
+
+  // Calculate the location of the upper left pixel.
+  Vec3 viewport_upper_left = sub_vec3(
+      sub_vec3(
+          sub_vec3(camera_center, new_vec(0, 0, focal_length)),
+          div_vec3(viewport_u, double2vec(2.0))
+      ),
+      div_vec3(viewport_v, double2vec(2.0))
+  );
+  Vec3 pixel00_loc = add_vec3(
+      viewport_upper_left,
+      mul_vec3(double2vec(0.5), add_vec3(pixel_delta_u, pixel_delta_v))
+  );
+}
 
 Ray get_ray(Camera* camera, int i, int j) {
   Vec3 offset = sample_sq();
