11bc21cdebb5b61514e21d2fa5a7f372e3e05aba
heabeounMKTO <heabeoun69zin@gmail.com>
2024-09-19 17:48:02 +0700

    cam func

--- src/camera.h
+++ src/camera.h
@@ -1,9 +1,36 @@
+#include "hitable_list.h"
 #include "ray.h"
 #include "utils.h"
 #include "vec.h"
 
+typedef struct {
+  int width, height;
+  double focal_length,viewport_height;
+  Vec3_d camera_center;
+} CameraSettings ;
 
 
+static inline void render(const CameraSettings* cam, const HitableList* world) {
+  double aspect_ratio = (double) cam->width / (double) cam->height;
+  double viewport_width = cam->viewport_height * aspect_ratio;
+  Vec3_d viewport_u = vec3d_new(viewport_width, 0.0, 0.0);
+  Vec3_d viewport_v = vec3d_new(0.0, -cam->viewport_height, 0.0);
+
+  Vec3_d pixel_delta_u = vec3d_div(viewport_u, vec3d_from_int(cam->width));
+  Vec3_d pixel_delta_v = vec3d_div(viewport_v, vec3d_from_int(cam->height));
+  Vec3_d viewport_upper_left = vec3d_sub(
+      vec3d_sub(vec3d_sub(cam->camera_center, vec3d_new(0.0, 0.0, cam->focal_length)),
+                vec3d_div(viewport_u, vec3d_from_float(2.0))),
+      vec3d_div(viewport_v, vec3d_from_float(2.0)));
+
+  Vec3_d pixel00_loc;
+  pixel00_loc = vec3d_add(viewport_upper_left,
+                          vec3d_mul(vec3d_from_float(0.5),
+                                    vec3d_add(pixel_delta_u, pixel_delta_v)));
+  
+
+
+}
 
 
 static inline Vec3_d sample_square() {
