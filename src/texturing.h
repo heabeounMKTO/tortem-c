9a8b1e2bbb1e871643583ec6178836c2b2db0fe2
heabeounMKTO <heabeoun69zin@gmail.com>
2024-09-24 16:30:35 +0700

    stash

---
+++ src/texturing.h
@@ -0,0 +1,20 @@
+#ifndef TEXTURING_H
+#define TEXTURING_H
+#include "vec.h"
+#include "color.h"
+
+
+typedef struct  {
+  Vec3_d albedo;
+} Texture;
+
+
+static inline Texture checker_texture(double scale, const Vec3_d color1, const Vec3_d color2) {
+
+}
+
+
+
+#endif
+
+
