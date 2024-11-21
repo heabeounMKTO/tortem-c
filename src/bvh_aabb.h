cb782105fa10de19975658f083f5829f8d444264
heabeounMKTO <heabeoun69zin@gmail.com>
2024-11-21 16:13:23 +0700

    bitch ass lunarvim so SLOW FUCK

--- src/bvh_aabb.h
+++ src/bvh_aabb.h
@@ -8,8 +8,14 @@ typedef struct {
   Interval x,y,z;
 } BvhAABB;
 
+static inline BvhAABB new_bvh(Interval x, Interval y, Interval z) {
+  return (BvhAABB) { .x=x, .y=y, .z=z };
+}
 
-
-
+static inline Interval axis_interval(BvhAABB bvh, int n) {
+  if (n == 1) return bvh.x;
+  if (n == 2) return bvh.z;
+  return bvh.x;
+}
 
 #endif
