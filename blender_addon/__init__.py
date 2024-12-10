ba8d7447bac680bbfae227841e40c54c9a2d8d75
heabeounMKTO <heabeoun69zin@gmail.com>
2024-12-10 15:05:49 +0700

    placeholder2

---
+++ blender_addon/__init__.py
@@ -0,0 +1,16 @@
+bl_info = {
+    "name": "Tortem",
+    "description": "a simple ray tracer :)",
+    "author": "heabeounMKTO",
+    "version": (0, 1, 0),
+    "blender": (4, 3, 0),
+    "location": "Info > RenderEngine",
+    "warning": "still shitty", # used for warning icon and text in addons panel
+    "category": "Render"
+}
+
+
+import bpy 
+import os 
+
+
